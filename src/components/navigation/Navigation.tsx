6b51cfab33525091384b3b40f6fa141720ce27a6
Ovett Mora <ovettmora17@gmail.com>
2024-08-19 16:45:32 -0600

    chore: Update navigation button in Navigation component

--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -54,8 +54,10 @@ export default function Navigation() {
           </div>
           <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
             <Button variant="contained" startIcon={<Add />}>
-              Send
+              <span>Add</span>
             </Button>
+
+            {/* Notification button */}
             <button
               type="button"
               className="relative rounded-full ml-3 bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
