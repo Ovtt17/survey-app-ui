12996592ca3e77b967153feb9cc5ff1565afc59c
Ovett Mora <ovettmora17@gmail.com>
2024-08-20 16:10:43 -0600

    feat: Add login functionality and login page

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -3,11 +3,14 @@ import { Route, Routes } from 'react-router-dom';
 import Home from '../pages/Home';
 import AnswerSurvey from '../components/survey/AnswerSurvey';
 import CreateSurveyForm from '../components/survey/CreateSurveyForm';
+import Login from '../components/user/Login';
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path='/login' element={ <Login /> } />
+      <Route path="/surveys" element={<Home />} />
       <Route path='/surveys/create' element={ <CreateSurveyForm /> } />
       <Route path="/surveys/:id/answer" element={<AnswerSurvey /> } />
     </Routes>
