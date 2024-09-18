73a5cb8d38933faa4bec30585b6976131e7553eb
Ovett Mora <ovettmora17@gmail.com>
2024-09-17 22:25:41 -0600

    refactor: Update App component to conditionally hide navigation and footer
    - Import the `useLocation` hook from `react-router-dom`
    - Add `useLocation` hook to get the current location
    - Create a variable `hideNavigationAndFooter` to check if the current path is '/register' or '/login'
    - Conditionally render the `Navigation` component and footer based on the `hideNavigationAndFooter` variable

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,29 @@
+import { useLocation } from "react-router-dom";
 import Navigation from "./components/navigation/Navigation";
 import { AuthProvider } from "./context/AuthContext";
 import AppRoutes from './routes/routes';
 import ScrollToTop from "./utils/ScrollToTop";
 
 function App() {
+  const location = useLocation();
+  const hideNavigationAndFooter = ['/register', '/login'].includes(location.pathname);
   return (
     <AuthProvider>
       <div className="min-h-screen flex flex-col">
         <ScrollToTop />
-        <header>
-          <Navigation />
-        </header>
+        {!hideNavigationAndFooter && (
+          <header>
+            <Navigation />
+          </header>
+        )}
         <main className="flex-grow p-4 bg-[#F0F4F9]">
           <AppRoutes />
         </main>
-        <footer className="bg-gray-800 p-4">
-          {/* Aquí puedes agregar el contenido del pie de página */}
-        </footer>
+        {!hideNavigationAndFooter && (
+          <footer className="bg-gray-800 p-4">
+            {/* Aquí puedes agregar el contenido del pie de página */}
+          </footer>
+        )}
       </div>
     </AuthProvider>
   );
