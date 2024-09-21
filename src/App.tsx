757643aca6ed461db7a3550aa8a3abad89dab0c2
Ovett Mora <ovettmora17@gmail.com>
2024-09-21 13:14:49 -0600

    Refactor: Remove Suspense fallback in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import { Suspense, lazy } from 'react';
+import { lazy } from 'react';
 import { useLocation } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
 import ScrollToTop from "./utils/ScrollToTop";
+import Navigation from './components/navigation/Navigation';
 
-const Navigation = lazy(() => import('./components/navigation/Navigation'));
 const AppRoutes = lazy(() => import('./routes/routes'));
 
 function App() {
@@ -13,20 +13,19 @@ function App() {
     <AuthProvider>
       <div className="min-h-screen flex flex-col">
         <ScrollToTop />
-        <Suspense fallback={<div>Loading...</div>}></Suspense>
-        {!hideNavigationAndFooter && (
-          <header>
-            <Navigation />
-          </header>
-        )}
-        <main className="flex-grow bg-body">
-          <AppRoutes />
-        </main>
-        {!hideNavigationAndFooter && (
-          <footer className="bg-gray-800 p-4">
-            {/* Aquí puedes agregar el contenido del pie de página */}
-          </footer>
-        )}
+          {!hideNavigationAndFooter && (
+            <header>
+              <Navigation />
+            </header>
+          )}
+          <main className="flex-grow bg-body">
+            <AppRoutes />
+          </main>
+          {!hideNavigationAndFooter && (
+            <footer className="bg-gray-800 p-4">
+              {/* Aquí puedes agregar el contenido del pie de página */}
+            </footer>
+          )}
       </div>
     </AuthProvider>
   );
