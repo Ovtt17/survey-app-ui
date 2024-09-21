703b5b23edfda56b5fc5d04ac20bd63bef0d0c4c
Ovett Mora <ovettmora17@gmail.com>
2024-09-21 13:43:23 -0600

    Refactor: Update LoadingComponent to use absolute positioning for the container

--- src/components/loadings/LoadingComponent.tsx
+++ src/components/loadings/LoadingComponent.tsx
@@ -2,7 +2,7 @@ import LoadingIndicator from './LoadingIndicator';
 
 const LoadingComponent = () => {
   return (
-    <div className="fixed inset-0 bg-white bg-opacity-100 flex items-center justify-center z-50">
+    <div className="absolute inset-0 bg-white bg-opacity-100 flex items-center justify-center z-50">
       <LoadingIndicator />
     </div>
   );
