d81ec20813f15092c0a6c1b9e6aa540901a1f806
Ovett Mora <ovettmora17@gmail.com>
2024-09-21 02:26:06 -0600

    Refactor: Adjust margin in OrSeparator component

--- src/components/sign-in/OrSeparator.tsx
+++ src/components/sign-in/OrSeparator.tsx
@@ -2,7 +2,7 @@ import { Box, Divider, Typography } from '@mui/material';
 
 const OrSeparator = () => {
   return (
-    <Box display="flex" alignItems="center" marginY={2}>
+    <Box display="flex" alignItems="center" marginY={1}>
       <Divider style={{ flex: 1 }} />
       <Typography variant="body1" style={{ margin: '0 10px' }}>or</Typography>
       <Divider style={{ flex: 1 }} />
