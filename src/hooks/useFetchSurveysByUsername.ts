e77e5d97c1802508926708e56411125d93e0be27
Ovett Mora <ovettmora17@gmail.com>
2024-09-27 23:10:01 -0600

    Refactor: Update useFetchSurveysByUsername and useFetchSurveysForOwner hooks

--- src/hooks/useFetchSurveysByUsername.ts
+++ src/hooks/useFetchSurveysByUsername.ts
@@ -1,8 +1,30 @@
+import { startTransition, useEffect, useState } from "react";
+import { Survey } from "../types/survey";
 import { getSurveysByUsername } from "../services/surveyService";
-import useFetchSurveys from "./useFetchSurveys";
 
 const useFetchSurveysByUsername = (username: string) => {
-  const fetchSurveysByUsername = () => getSurveysByUsername(username);
-  return useFetchSurveys(fetchSurveysByUsername);
+  const [surveys, setSurveys] = useState<Survey[]>([]);
+  const [errorMessage, setErrorMessage] = useState<string>('');
+  const [openErrorTemplate, setOpenErrorTemplate] = useState(false);
+
+  useEffect(() => {
+    const fetchSurveys = async () => {
+      try {
+        const fetchedSurvey = await getSurveysByUsername(username);
+        setSurveys(fetchedSurvey);
+      } catch (error) {
+        const errorMessage = error instanceof Error ? error.message : 'Error desconocido';
+        startTransition(() => {
+          setErrorMessage(errorMessage);
+          setOpenErrorTemplate(true);
+        });
+      }
+    };
+
+    fetchSurveys();
+  }, []);
+
+  return { surveys, errorMessage, openErrorTemplate, setSurveys, setOpenErrorTemplate };
 };
+
 export default useFetchSurveysByUsername;
--- src/hooks/useFetchSurveysForOwner.ts
+++ src/hooks/useFetchSurveysForOwner.ts
@@ -1,8 +1,30 @@
+import { useState, useEffect, startTransition } from 'react';
 import { getCurrentUserSurveys } from '../services/surveyService';
-import useFetchSurveys from './useFetchSurveys';
+import { Survey } from '../types/survey';
 
 const useFetchSurveysForOwner = () => {
-  return useFetchSurveys(getCurrentUserSurveys);
+  const [surveys, setSurveys] = useState<Survey[]>([]);
+  const [errorMessage, setErrorMessage] = useState<string>('');
+  const [openErrorTemplate, setOpenErrorTemplate] = useState(false);
+
+  useEffect(() => {
+    const fetchSurveys = async () => {
+      try {
+        const fetchedSurvey = await getCurrentUserSurveys();
+        setSurveys(fetchedSurvey);
+      } catch (error) {
+        const errorMessage = error instanceof Error ? error.message : 'Error desconocido';
+        startTransition(() => {
+          setErrorMessage(errorMessage);
+          setOpenErrorTemplate(true);
+        });
+      }
+    };
+
+    fetchSurveys();
+  }, []);
+
+  return { surveys, errorMessage, openErrorTemplate, setSurveys, setOpenErrorTemplate };
 };
 
 export default useFetchSurveysForOwner;
