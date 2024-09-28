7833187abc47c8e0ce809a374c52a87086a92cc9
Ovett Mora <ovettmora17@gmail.com>
2024-09-27 23:53:05 -0600

    Refactor: Add Rating component to ProfileSurveyCard

--- src/components/profile/ProfileSurveyCard.tsx
+++ src/components/profile/ProfileSurveyCard.tsx
@@ -1,6 +1,7 @@
 import { FC } from 'react';
 import { Survey } from '../../types/survey';
 import { Link } from 'react-router-dom';
+import { Rating } from '@mui/material';
 
 interface ProfileSurveyCardProps {
   survey: Survey;
@@ -8,10 +9,20 @@ interface ProfileSurveyCardProps {
 
 const ProfileSurveyCard: FC<ProfileSurveyCardProps> = ({ survey }) => {
   return (
-    <div className="bg-white border-2 rounded-lg px-2 py-4">
+    <div className="bg-white border-2 rounded-lg p-4">
       <Link to={`/surveys/${survey.id}`} className='text-blue-800 hover:underline'>
         <h3 className="font-medium">{survey.title}</h3>
       </Link>
+      <span className="text-gray-600 text-sm flex items-center">
+        Rating:
+        <Rating
+          name="read-only ml-1"
+          size="small"
+          value={survey.averageRating}
+          readOnly
+          precision={0.5}
+        />
+      </span>
       <p className="text-xs font-extralight text-gray-600">{survey.description}</p>
     </div>
   );
