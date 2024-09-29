6dbac4c4c69df991de86c3fe4a854f4ac9c65c7c
Ovett Mora <ovettmora17@gmail.com>
2024-09-29 14:50:08 -0600

    Refactor: Update ProfileAside component to use NoProfilePictureWhiteIcon as default profile picture

--- src/components/profile/ProfileAside.tsx
+++ src/components/profile/ProfileAside.tsx
@@ -1,6 +1,6 @@
 import { FC } from 'react';
 import useProfileUser from '../../hooks/useProfileUser';
-import AccountCircleRoundedIcon from '@mui/icons-material/AccountCircleRounded';
+import NoProfilePictureWhiteIcon from '../../assets/no-profile-picture-bg-white.svg';
 
 interface ProfileAsideProps {
 }
@@ -12,25 +12,11 @@ const ProfileAside: FC<ProfileAsideProps> = ({ }) => {
     <aside className="w-full md:w-1/5 md:max-w-1/3 md:pb-6">
       <div className="flex flex-col gap-6 items-center md:items-start">
         <div className="flex justify-center w-full">
-          {profileUser?.profilePictureUrl ? (
-            <img
-              src={profileUser?.profilePictureUrl}
-              alt="profile-picture"
-              className="min-w-24 min-h-24 max-w-64 max-h-64 w-full h-auto rounded-full object-cover"
-            />
-          ) : (
-              <div className="min-w-24 min-h-24 max-w-64 max-h-64 w-full h-auto rounded-full flex items-center justify-center bg-gray-200">
-                <AccountCircleRoundedIcon
-                  style={{
-                    fontSize: '256px',
-                    width: '100%',
-                    height: 'auto',
-                    maxWidth: '256px',
-                    maxHeight: '256px',
-                  }}
-                />
-              </div>
-          )}
+          <img
+            src={profileUser?.profilePictureUrl || NoProfilePictureWhiteIcon}
+            alt="profile-picture"
+            className="min-w-24 min-h-24 max-w-64 max-h-64 w-full h-auto rounded-full object-cover"
+          />
         </div>
         <div className="text-center md:text-left">
           <p className="text-2xl font-bold mb-2">{profileUser?.fullName}</p>
