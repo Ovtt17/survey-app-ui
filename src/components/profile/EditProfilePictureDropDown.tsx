6194497be607375f3ffa38393fb5d185c494044f
Ovett Mora <ovettmora17@gmail.com>
2024-09-30 21:48:15 -0600

    Refactor: Update EditProfilePictureDropDown component to include file upload functionality

--- src/components/profile/EditProfilePictureDropDown.tsx
+++ src/components/profile/EditProfilePictureDropDown.tsx
@@ -17,6 +17,20 @@ const EditProfilePictureDropDown: FC<EditProfilePictureDropDownProps> = ({ profi
     setMenuAnchorElement(null);
   };
 
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    if (file) {
+      const validExtensions = ['jpg', 'jpeg', 'png'];
+      const fileExtension = file.name.split('.').pop()?.toLowerCase();
+      if (fileExtension && validExtensions.includes(fileExtension)) {
+        console.log(file);
+      } else {
+        alert('Por favor, selecciona un archivo de imagen válido (jpg, jpeg, png).');
+      }
+      handleMenuClose();
+    }
+  };
+
   return (
     <div className='absolute border bottom-0 left-0 mb-5 ml-8 md:mb-3 md:ml-4 px-2 py-0.5 bg-midnight-black text-white rounded-md hover:bg-gray-700'>
       <button
@@ -41,8 +55,19 @@ const EditProfilePictureDropDown: FC<EditProfilePictureDropDownProps> = ({ profi
           horizontal: 'left',
         }}
       >
-        <MenuItem onClick={handleMenuClose} sx={{ fontSize: '14px' }}>Subir nueva foto</MenuItem>
-        {profilePicture && <MenuItem onClick={handleMenuClose} sx={{ fontSize: '14px' }}>Eliminar foto</MenuItem> }
+        <MenuItem sx={{ fontSize: '14px' }}>
+          <label htmlFor="upload-photo" style={{ cursor: 'pointer' }}>
+            Subir nueva foto
+          </label>
+          <input
+            type="file"
+            id="upload-photo"
+            accept="image/*"
+            style={{ display: 'none' }}
+            onChange={handleFileChange}
+          />
+        </MenuItem>
+        {profilePicture && <MenuItem onClick={handleMenuClose} sx={{ fontSize: '14px' }}>Eliminar foto</MenuItem>}
       </Menu>
     </div>
   );
