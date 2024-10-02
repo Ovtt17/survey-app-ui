020b49cd9219e12c5afb92096b0202736fe7092c
Ovett Mora <ovettmora17@gmail.com>
2024-10-01 18:08:31 -0600

    Refactor: Update UserMenuDropDown component to use @headlessui/react MenuItem

--- src/components/navigation/UserMenuDropDown.tsx
+++ src/components/navigation/UserMenuDropDown.tsx
@@ -1,8 +1,8 @@
-import { MenuItem } from '@mui/material';
 import { Link } from 'react-router-dom';
 import { useAuthContext } from '../../context/AuthContext';
+import { MenuItem } from '@headlessui/react';
 
-const UserMenuDropDown = () => {
+const UserMenuDropDown = ({ }) => {
   const { isAuthenticated, logout, user } = useAuthContext();
 
   return (
@@ -10,27 +10,27 @@ const UserMenuDropDown = () => {
       {isAuthenticated ? (
         <>
           <MenuItem>
-            <Link to={`/${user?.username}`} className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100">
+            <Link to={`/${user?.username}`} className="block w-full h-full px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100">
               Perfil
             </Link>
           </MenuItem>
           <MenuItem>
-            <Link to={`/${user?.username}/surveys`} className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100">
+            <Link to={`/${user?.username}/surveys`} className="block w-full h-full px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100">
               Mis Encuestas
             </Link>
           </MenuItem>
           <MenuItem>
-            <Link to={`/${user?.username}/reports`} className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100">
+            <Link to={`/${user?.username}/reports`} className="block w-full h-full px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100">
               Generar Reportes
             </Link>
           </MenuItem>
           <MenuItem>
-            <Link to="/settings" className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100">
+            <Link to="/settings" className="block w-full h-full px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100">
               Configuración
             </Link>
           </MenuItem>
           <MenuItem>
-            <Link to="/" onClick={logout} className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100">
+            <Link to="/" onClick={logout} className="block w-full h-full px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100">
               Cerrar Sesión
             </Link>
           </MenuItem>
@@ -38,12 +38,12 @@ const UserMenuDropDown = () => {
       ) : (
         <>
           <MenuItem>
-            <Link to="/login" className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100">
+            <Link to="/login" className="block w-full h-full px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100">
               Sign In
             </Link>
           </MenuItem>
           <MenuItem>
-            <Link to="/register" className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100">
+            <Link to="/register" className="block w-full h-full px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100">
               Register
             </Link>
           </MenuItem>
