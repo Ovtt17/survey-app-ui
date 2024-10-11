4bf7444f7b8ec1bbec8537acc49122800769a750
Ovett Mora <ovettmora17@gmail.com>
2024-10-10 20:57:58 -0600

    Refactor: Extract AccordionSummary component in AccordionItem.tsx

--- src/components/accordion/AccordionItem.tsx
+++ src/components/accordion/AccordionItem.tsx
@@ -1,12 +1,9 @@
 import React, { useState } from 'react';
 import Accordion from '@mui/material/Accordion';
 import AccordionDetails from '@mui/material/AccordionDetails';
-import AccordionSummary from '@mui/material/AccordionSummary';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import DeleteOutlineOutlinedIcon from '@mui/icons-material/DeleteOutlineOutlined'; import AccordionDetailsContent from './AccordionDetailsContent';
+ import AccordionDetailsContent from './AccordionDetailsContent';
 import { Question } from '../../types/question';
+import AccordionSummaryComponent from './AccordionSummaryComponent';
 
 interface AccordionItemProps {
     question: Question;
@@ -23,25 +20,7 @@ const AccordionItem: React.FC<AccordionItemProps> = ({ index, removeQuestion }) 
 
     return (
         <Accordion expanded={expanded} onChange={handleExpansion}>
-            <AccordionSummary
-                expandIcon={<ExpandMoreIcon sx={{ color: 'white'}}/>}
-                aria-controls={`panel${index}-content`}
-                id={`panel${index}-header`}
-                sx={{ backgroundColor: 'black', color: 'white' }}
-            >
-                <div className='flex items-center justify-between w-full'>
-                    <Typography>Pregunta {index + 1}</Typography>
-                    <IconButton
-                        aria-label="delete"
-                        onClick={(e) => {
-                            e.stopPropagation();
-                            removeQuestion();
-                        }}
-                    >
-                        <DeleteOutlineOutlinedIcon sx={{color: 'white'}}  />
-                    </IconButton>
-                </div>
-            </AccordionSummary>
+            <AccordionSummaryComponent index={index} removeQuestion={removeQuestion} />
             <AccordionDetails>
                 <AccordionDetailsContent questionIndex={index} />
             </AccordionDetails>
