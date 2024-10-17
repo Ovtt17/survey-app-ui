3f95e6697d614016abe955a009f699c94f08bd09
Ovett Mora <ovettmora17@gmail.com>
2024-10-17 15:40:35 -0600

    Refactor: Update ProfileSurveyCard component to display survey image or fallback to default image

--- src/components/profile/ProfileSurveyCard.tsx
+++ src/components/profile/ProfileSurveyCard.tsx
@@ -2,7 +2,7 @@ import { FC } from 'react';
 import {SurveyResponse} from '../../types/survey';
 import { Link } from 'react-router-dom';
 import { Rating } from '@mui/material';
-
+import imageNotAvailable from '../../assets/Image_not_available.png';
 interface ProfileSurveyCardProps {
   survey: SurveyResponse;
 }
@@ -10,13 +10,15 @@ interface ProfileSurveyCardProps {
 const ProfileSurveyCard: FC<ProfileSurveyCardProps> = ({ survey }) => {
   return (
     <article className="bg-white border-2 rounded-lg">
+      <Link to={`/surveys/${survey.id}`}>
       <figure className="max-md:h-32 h-36 flex-none rounded-t overflow-hidden">
         <img
-          src="https://plus.unsplash.com/premium_photo-1673306778968-5aab577a7365?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8YmFja2dyb3VuZCUyMGltYWdlfDB8fDB8fHww"
+          src={survey.pictureUrl || imageNotAvailable}
           alt={`Portada de la encuesta ${survey.title}`}
           className="w-full h-full object-cover"
         />
       </figure>
+      </Link>
       <section className="p-4">
         <Link to={`/surveys/${survey.id}`} className="text-blue-800 hover:underline">
           <h3 className="font-medium">{survey.title}</h3>
