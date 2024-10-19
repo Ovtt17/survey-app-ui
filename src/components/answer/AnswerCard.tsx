db90e30da52955c93097fb268c790636d92a1dae
Ovett Mora <ovettmora17@gmail.com>
2024-10-18 19:15:06 -0600

    Refactor: Update AnswerCard component to handle different question types and add profile picture support

--- src/components/answer/AnswerCard.tsx
+++ src/components/answer/AnswerCard.tsx
@@ -1,12 +1,17 @@
 import { FC } from 'react';
-import FormControl from '@mui/material/FormControl';
-import FormLabel from '@mui/material/FormLabel';
-import RadioGroup from '@mui/material/RadioGroup';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Radio from '@mui/material/Radio';
-import Typography from '@mui/material/Typography';
+import {
+  FormControl,
+  FormLabel,
+  RadioGroup,
+  FormControlLabel,
+  Radio,
+  Typography,
+  TextField,
+  Paper,
+} from '@mui/material';
 import { Question } from '../../types/question';
 import { Answer } from '../../types/answer';
+import { QuestionType } from '../../types/questionType';
 
 interface AnswerCardProps {
   surveyId: number;
@@ -16,40 +21,67 @@ interface AnswerCardProps {
   handleAnswerChange: (questionId: number, answer: Answer) => void;
 }
 
-const AnswerCard: FC<AnswerCardProps> = ({ surveyId, question, answers, unansweredQuestions, handleAnswerChange }) => {
+const AnswerCard: FC<AnswerCardProps> = ({
+  surveyId,
+  question,
+  answers,
+  unansweredQuestions,
+  handleAnswerChange,
+}) => {
+  const handleRadioChange = (optionText: string) => {
+    handleAnswerChange(question.id || 0, { surveyId, questionId: question.id || 0, answerText: optionText });
+  };
+
+  const handleTextChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    handleAnswerChange(question.id || 0, { surveyId, questionId: question.id || 0, answerText: event.target.value });
+  };
+
   return (
-    <div
-      className={`border rounded-lg shadow-md my-5 p-5 ${unansweredQuestions.includes(question.id || 0) ? 'bg-red-100 border-red-500' : 'bg-slate-200'}`}
-      key={question.id}
+    <Paper
+      elevation={3}
+      className={`my-5 p-5 ${unansweredQuestions.includes(question.id || 0) ? 'bg-red-100 border-red-500' : 'bg-slate-200'}`}
     >
-      <FormControl component="fieldset">
+      <FormControl component="fieldset" fullWidth>
         <FormLabel component="legend">
           <Typography variant="h6" gutterBottom>
             {question.text}
           </Typography>
         </FormLabel>
         <RadioGroup>
-          {question.options?.map((option) => (
-            <FormControlLabel
-              key={option.id}
-              value={option.id}
-              control={
-                <Radio
-                  checked={answers.some(
-                    (a) => a.questionId === question.id && a.answerText === option.text
-                  )}
-                  onChange={() =>
-                    handleAnswerChange(question.id || 0, { surveyId: surveyId, questionId: question.id || 0, answerText: option.text })
+          {question.type === QuestionType.SELECCION_UNICA && (
+            <>
+              {question.options?.map((option) => (
+                <FormControlLabel
+                  key={option.id}
+                  value={option.id}
+                  control={
+                    <Radio
+                      checked={answers.some((a) => a.questionId === question.id && a.answerText === option.text)}
+                      onChange={() => handleRadioChange(option.text)}
+                    />
                   }
+                  label={<Typography variant="body1">{option.text}</Typography>}
                 />
-              }
-              label={option.text}
+              ))}
+            </>
+          )}
+          {question.type === QuestionType.TEXTO && (
+            <TextField
+              fullWidth
+              variant="outlined"
+              value={answers.find((a) => a.questionId === question.id)?.answerText || ''}
+              onChange={handleTextChange}
+              label="Your answer"
+              margin="normal"
+              multiline
+              rows={4}
+              helperText="Please provide your response."
             />
-          ))}
+          )}
         </RadioGroup>
       </FormControl>
-    </div>
+    </Paper>
   );
-}
+};
 
 export default AnswerCard;
