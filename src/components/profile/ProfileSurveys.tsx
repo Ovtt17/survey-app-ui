f7a5c63274b81534ca8e1104e320b67d441beeb2
Ovett Mora <ovettmora17@gmail.com>
2024-10-20 19:31:12 -0600

    Refactor: Replace SurveyCard with SurveyList in ProfileSurveys component

--- src/components/profile/ProfileSurveys.tsx
+++ src/components/profile/ProfileSurveys.tsx
@@ -1,16 +1,11 @@
-import { FC, useState } from 'react';
+import { useState } from 'react';
 import useFetchSurveysByCurrentUser from '../../hooks/useFetchSurveysByCurrentUser';
 import { useParams } from 'react-router-dom';
 import { useAuthContext } from '../../context/AuthContext';
 import useFetchSurveysByUsername from '../../hooks/useFetchSurveysByUsername';
-import { Pagination } from '@mui/material';
-import SurveyCard from '../survey/SurveyCard';
+import SurveyList from '../survey/SurveyList';
 
-interface ProfileSurveysProps {
-
-}
-
-const ProfileSurveys: FC<ProfileSurveysProps> = ({ }) => {
+const ProfileSurveys = () => {
   const { username } = useParams<{ username: string }>();
   const { isProfileOwner } = useAuthContext();
   const [page, setPage] = useState(1);
@@ -34,40 +29,18 @@ const ProfileSurveys: FC<ProfileSurveysProps> = ({ }) => {
       <h2 className="text-xl font-semibold mb-4 border-b-2">Encuestas</h2>
       {hasSurveys ? (
         <>
-          <div className="flex-grow">
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-              {surveys.map((survey) => (
-                <SurveyCard
-                  key={survey.id}
-                  survey={survey}
-                  allowOwnerOptions
-                  onDelete={handleSurveyDeleted}
-                />
-              ))}
-            </div>
-          </div>
-          <div className="flex justify-center mt-5 mb-16 md:my-0">
-            <Pagination
-              count={totalPages}
-              page={page}
-              shape="rounded"
-              size='small'
-              sx={{
-                '& .MuiPaginationItem-root': {
-                  '&.Mui-selected': {
-                    backgroundColor: 'var(--tw-bg-midnight-black)',
-                    color: '#FFFFFF',
-                  },
-                },
-              }}
-              onChange={handleSurveyPageChange}
-            />
-          </div>
+          <SurveyList
+            surveys={surveys}
+            totalPages={totalPages}
+            page={page}
+            onPageChange={handleSurveyPageChange}
+            handleSurveyDeleted={handleSurveyDeleted}
+          />
         </>
       ) : openErrorTemplate ? (
         <div className="text-red-500">Error al cargar las encuestas.</div>
       ) : (
-        <div className="text-gray-500">No tienes encuestas.</div>
+        <div className="text-gray-500">{isOwner ? 'No tienes encuestas.' : 'No hay encuestas'}</div>
       )}
     </div>
   );
