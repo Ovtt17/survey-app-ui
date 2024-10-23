6d2b6fe19a3cb612fb9e5be04ee86927db6ec798
Ovett Mora <ovettmora17@gmail.com>
2024-10-23 16:52:33 -0600

    Refactor: Update UserSurveys component to use SurveysGlimmer for loading state and SurveyList for displaying surveys

--- src/pages/UserSurveys.tsx
+++ src/pages/UserSurveys.tsx
@@ -1,12 +1,11 @@
-import SurveyCard from "../components/survey/SurveyCard";
 import ErrorTemplate from "../components/error/ErrorTemplate";
 import { useNavigate } from "react-router-dom";
 import useFetchSurveysByCurrentUser from "../hooks/useFetchSurveysByCurrentUser";
 import { useState } from "react";
-import { Pagination } from "@mui/material";
-import LoadingComponent from "../components/loadings/LoadingComponent";
 import CreateSurveyButton from "../components/buttons/CreateSurveyButton";
 import ErrorModal from "../components/error/ErrorModal";
+import SurveysGlimmer from "../components/loadings/SurveysGlimmer";
+import SurveyList from "../components/survey/SurveyList";
 
 const UserSurveys = () => {
   const navigate = useNavigate();
@@ -29,11 +28,9 @@ const UserSurveys = () => {
   const {
     surveys,
     setSurveys,
-    errorMessage,
-    openErrorTemplate,
-    setOpenErrorTemplate,
     totalPages,
-    loading
+    loading,
+    error
   } = useFetchSurveysByCurrentUser(page, pageSize);
 
   const handleSurveyDeleted = (id: number) => {
@@ -42,67 +39,49 @@ const UserSurveys = () => {
 
   const hasSurveys = surveys.length > 0;
 
+  if (loading) {
+    return (
+      <div className="flex flex-col items-center">
+        <SurveysGlimmer />
+      </div>
+    )
+  }
+  if (error) {
+    return (
+      <ErrorTemplate
+        title="Encuestas no encontradas."
+        message={error}
+        buttonText="Regresar al inicio"
+        onButtonClick={() => {
+          navigate("/");
+        }}
+      />
+    );
+  }
+
   return (
-    <div>
+    <div className="flex flex-col justify-center">
       <div className="flex flex-col items-center">
         <h2 className="text-2xl font-bold">Mis Encuestas</h2>
       </div>
-
-      {loading ? (
-        <LoadingComponent />
-      ) : hasSurveys && !openErrorTemplate ? (
-        <div>
-          <div className="flex flex-wrap justify-start">
-            {surveys?.map((survey, index) => {
-              return (
-                <div key={index} className="w-full sm:w-1/2 p-2">
-                  <SurveyCard
-                    survey={survey}
-                    allowOwnerOptions
-                    onDelete={handleSurveyDeleted}
-                  />
-                </div>
-              );
-            })}
-          </div>
-          <div className="flex justify-center mt-5 mb-16 md:my-0">
-            <Pagination
-              count={totalPages}
-              page={page}
-              shape="rounded"
-              size='small'
-              sx={{
-                '& .MuiPaginationItem-root': {
-                  '&.Mui-selected': {
-                    backgroundColor: 'var(--tw-bg-midnight-black)',
-                    color: '#FFFFFF',
-                  },
-                },
-              }}
-              onChange={handleSurveyPageChange}
-            />
-          </div>
-            <CreateSurveyButton handleOpenErrorModal={() => handleOpenErrorModal} />
-            <ErrorModal
-              open={openErrorModal}
-              setOpen={setOpenErrorModal}
-              title="Error"
-              message="Para realizar esta acción es necesario iniciar sesión"
-              confirmText="Iniciar Sesión"
-              onConfirm={handleConfirmLogin}
-            />
-        </div>
-      ) : (
-        <ErrorTemplate
-          title="Encuestas no encontradas."
-          message={errorMessage}
-          buttonText="Regresar al inicio"
-          onButtonClick={() => {
-            setOpenErrorTemplate(false);
-            navigate("/");
-          }}
+      {hasSurveys && (
+        <SurveyList
+          surveys={surveys}
+          totalPages={totalPages}
+          page={page}
+          onPageChange={handleSurveyPageChange}
+          handleSurveyDeleted={handleSurveyDeleted}
         />
       )}
+      <CreateSurveyButton handleOpenErrorModal={() => handleOpenErrorModal} />
+      <ErrorModal
+        open={openErrorModal}
+        setOpen={setOpenErrorModal}
+        title="Error"
+        message="Para realizar esta acción es necesario iniciar sesión"
+        confirmText="Iniciar Sesión"
+        onConfirm={handleConfirmLogin}
+      />
     </div>
   );
 }
