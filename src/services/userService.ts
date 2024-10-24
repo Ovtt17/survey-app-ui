acdc82edab6f5e4347300b8aae2b4dd3ea656825
Ovett Mora <ovettmora17@gmail.com>
2024-10-23 21:29:27 -0600

    Refactor userService to use fetchWithHandling from networkService for error handling

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,8 +1,6 @@
-import { AnimationPaths } from "../constants/animationPaths";
-import { AppError } from "../types/AppError";
 import { User } from "../types/user";
 import { getToken } from "../utils/auth";
-import { handleErrorResponse } from "./handleErrorResponse";
+import { fetchWithHandling } from "./networkService";
 
 const BASE_URL = `${import.meta.env.VITE_API_URL}/users`;
 
@@ -13,43 +11,15 @@ const getHeaders = () => ({
 });
 
 export const getUser = async (): Promise<User> => {
-  try {
-    const response = await fetch(BASE_URL + '/me', {
-      method: 'GET',
-      headers: getHeaders()
-    });
-    if (!response.ok) {
-      throw new Error(`Failed to fetch user data: ${response.statusText}`);
-    }
-    const user: User = await response.json();
-    return user;
-  } catch (error) {
-    console.error('Error fetching user data:', error);
-    throw new Error('An error occurred while fetching user data. Please try again later.');
-  }
+  return await fetchWithHandling(BASE_URL + '/me', {
+    method: 'GET',
+    headers: getHeaders()
+  });
 };
 
 export const getUserByUsername = async (username: string): Promise<User> => {
-  try {
-    const response = await fetch(BASE_URL + `/${username}`, {
-      method: 'GET',
-      headers: getHeaders()
-    });
-    if (!response.ok) {
-      await handleErrorResponse(response);
-    }
-    const user: User = await response.json();
-    return user;
-  } catch (error) {
-    if (error instanceof TypeError) {
-      throw new AppError(
-        'Servidor no disponible',
-        'No se pudo conectar con el servidor. Por favor, inténtelo de nuevo más tarde.',
-        AnimationPaths.ServerUnavailable,
-        'Volver al inicio'
-      );
-    }
-    console.error('Error during fetch operation:', error);
-    throw error;
-  }
+  return await fetchWithHandling(BASE_URL + `/${username}`, {
+    method: 'GET',
+    headers: getHeaders()
+  });
 };
