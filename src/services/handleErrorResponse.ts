9736dbaa1b352609385d5939bff02fed2751e9a8
Ovett Mora <ovettmora17@gmail.com>
2024-10-23 20:43:29 -0600

    Add handleErrorResponse

---
+++ src/services/handleErrorResponse.ts
@@ -0,0 +1,23 @@
+import { errorMappings } from "../constants/errorMappings";
+import { AppError } from "../types/AppError";
+import { ExceptionResponse } from "../types/ExceptionResponse";
+
+export const handleErrorResponse = async (response: Response) => {
+  const errorData: ExceptionResponse = await response.json();
+  const errorMapping = errorMappings[response.status];
+  if (errorMapping) {
+    throw new AppError(
+      errorData.businessErrorDescription || 'Error',
+      errorData.error || 'Error desconocido',
+      errorMapping.animationSrc,
+      errorMapping.buttonText
+    );
+  } else {
+    throw new AppError(
+      'Error desconocido',
+      errorData.error || 'Error desconocido',
+      '../../assets/lottie/NoResultFound.lottie',
+      'Volver al inicio'
+    );
+  }
+};
