0ce03e041c74a955d726de243b77db2dddcf6438
Ovett Mora <ovettmora17@gmail.com>
2024-10-23 20:12:44 -0600

    Refactor useFetchSurveys to handle AppError object for error data

--- src/hooks/useFetchSurveys.ts
+++ src/hooks/useFetchSurveys.ts
@@ -1,10 +1,11 @@
 import { useState, useEffect } from "react";
 import {SurveyResponse} from "../types/survey";
 import { getSurveys } from "../services/surveyService";
+import { AppError } from "../types/AppError";
 
 const useFetchSurveys = (page: number, size: number) => {
   const [surveys, setSurveys] = useState<SurveyResponse[]>([]);
-  const [error, setError] = useState<string>('');
+  const [error, setError] = useState<AppError | null>(null);
   const [currentPage, setCurrentPage] = useState(page);
   const [totalPages, setTotalPages] = useState(0);
   const [loading, setLoading] = useState(true);
@@ -18,7 +19,7 @@ const useFetchSurveys = (page: number, size: number) => {
         setCurrentPage(response.page + 1);
         setTotalPages(response.totalPages);
       } catch (error: unknown) {
-        setError((error as Error).message);
+        setError(error as AppError);
       } finally {
         setLoading(false);
       }
