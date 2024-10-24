29d33586e16bbf6b81a663175a1e84c6e58d5702
Ovett Mora <ovettmora17@gmail.com>
2024-10-23 20:36:23 -0600

    Refactor ErrorTemplate to display error title in red color

--- src/components/error/ErrorTemplate.tsx
+++ src/components/error/ErrorTemplate.tsx
@@ -17,7 +17,7 @@ const ErrorTemplate: React.FC<ErrorTemplateProps> = ({ error, onButtonClick }) =
         autoplay
         className="w-[250px] sm:w-[300px] md:w-[400px] h-[250px] sm:h-[300px] md:h-[330px]"
       />
-      <h1 className="text-2xl font-bold sm:text-3xl md:text-4xl leading-snug text-blue-gray-900">
+      <h1 className="text-2xl text-red-500 font-bold sm:text-3xl md:text-4xl leading-snug text-blue-gray-900">
         {error.title}
       </h1>
       <p className="text-base sm:text-lg md:text-xl">
