7519e236ee2a34d4b82a551d41754a2795b6dead
Ovett Mora <ovettmora17@gmail.com>
2024-10-24 21:33:00 -0600

    refactor: useFetchSurveysByCurrentUser to handle AppError instead of string

--- src/hooks/useFetchSurveysByCurrentUser.ts
+++ src/hooks/useFetchSurveysByCurrentUser.ts
@@ -1,13 +1,14 @@
 import { useState, useEffect } from 'react';
 import { getSurveysByCurrentUserWithPaging } from '../services/surveyService';
 import {SurveyResponse} from '../types/survey';
+import { AppError } from '../types/AppError';
 
 const useFetchSurveysByCurrentUser = (page: number, size: number) => {
   const [surveys, setSurveys] = useState<SurveyResponse[]>([]);
   const [currentPage, setCurrentPage] = useState(page);
   const [totalPages, setTotalPages] = useState(0);
   const [loading, setLoading] = useState(true);
-  const [error, setError] = useState<string>('');
+  const [error, setError] = useState<AppError | null>(null);
 
   useEffect(() => {
     const fetchSurveys = async () => {
@@ -17,8 +18,7 @@ const useFetchSurveysByCurrentUser = (page: number, size: number) => {
         setCurrentPage(response.page + 1);
         setTotalPages(response.totalPages);
       } catch (error: unknown) {
-        const errorMessage = error instanceof Error ? error.message : 'Error desconocido';
-        setError(errorMessage);
+        setError(error as AppError);
       } finally {
         setLoading(false);
       }
