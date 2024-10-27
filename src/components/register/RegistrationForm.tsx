f66cc447bd9444b962c22495dabea099d59f11d8
Ovett Mora <ovettmora17@gmail.com>
2024-10-27 00:42:43 -0600

    Refactor RegistrationForm component to use react-hook-form for form management

--- src/components/register/RegistrationForm.tsx
+++ src/components/register/RegistrationForm.tsx
@@ -2,81 +2,50 @@ import React, { FC } from 'react';
 import PersonalDetailsStep from './PersonalDetailsStep';
 import PhoneAndDateOfBirthStep from './PhoneAndDateOfBirthStep';
 import EmailStep from './EmailStep';
-import { NewUser } from '../../types/user';
-import { Dayjs } from 'dayjs';
-import { StepErrors } from '../../auth/constants';
-import ErrorMessage from './ErrorMessage';
 import StepNavigation from './StepNavigation';
 import UsernameStep from './UsernameStep';
 import PasswordStep from './PasswordStep';
+import { useFormContext } from 'react-hook-form';
 
 interface RegistrationFormProps {
-  formData: NewUser;
-  errors: Array<StepErrors>;
-  dateOfBirth: Dayjs | null;
-  minDate: Dayjs;
-  maxDate: Dayjs;
-  errorMessage: string;
-  step: number;
-  handleChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
-  handleChangeDate: (newDateOfBirth: Dayjs | null) => void;
-  updateError: (field: keyof StepErrors, value: string | null) => void;
-  handlePrevStep: () => void;
-  handleNextStep: () => void;
-  handleSubmit: (event: React.FormEvent<HTMLFormElement>) => void;
+  handleSubmit: (event: React.FormEvent) => void;
 }
 
 const RegistrationForm: FC<RegistrationFormProps> = ({
-  formData,
-  errors,
-  dateOfBirth,
-  minDate,
-  maxDate,
-  errorMessage,
-  step,
-  handleChange,
-  handleChangeDate,
-  updateError,
-  handlePrevStep,
-  handleNextStep,
   handleSubmit,
 }) => {
+  const { trigger } = useFormContext();
+  const [step, setStep] = React.useState<number>(0);
   const steps = [
     <PersonalDetailsStep
-      firstName={formData.firstName}
-      lastName={formData.lastName}
-      handleChange={handleChange}
-      firstNameError={errors[0].firstName}
-      lastNameError={errors[0].lastName}
     />,
     <PhoneAndDateOfBirthStep
-      phone={formData.phone}
-      dateOfBirth={dateOfBirth}
-      handleChange={handleChange}
-      handleChangeDate={handleChangeDate}
-      setFieldError={updateError}
-      phoneError={errors[1].phone}
-      minDate={minDate}
-      maxDate={maxDate}
     />,
     <EmailStep
-      email={formData.email}
-      handleChange={handleChange}
-      emailError={errors[2].email}
     />,
     <UsernameStep
-      username={formData.username}
-      handleChange={handleChange}
-      usernameError={errors[3].username}
     />,
     <PasswordStep
-      password={formData.password}
-      confirmPassword={formData.confirmPassword}
-      handleChange={handleChange}
-      passwordError={errors[4].password}
-      confirmPasswordError={errors[4].confirmPassword}
     />
   ];
+
+  const validateStep = async () => {
+    const result = await trigger(['firstName', 'lastName']);
+    return result;
+  }
+
+  const handlePrevStep = () => {
+    setStep(step - 1);
+  }
+
+  const handleNextStep = async () => {
+    const isValidStep = await validateStep();
+    if (!isValidStep) {
+      return;
+    }
+    setStep(step + 1);
+  };
+
 
   const totalSteps = steps.length;
 
@@ -85,7 +54,6 @@ const RegistrationForm: FC<RegistrationFormProps> = ({
       <div>
         {steps[step]}
       </div>
-      {errorMessage && <ErrorMessage message={errorMessage} />}
       <StepNavigation
         step={step}
         totalSteps={totalSteps}
