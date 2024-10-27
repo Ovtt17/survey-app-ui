3b1568ed46c0adc747c22303a92292923b6f3cc5
Ovett Mora <ovettmora17@gmail.com>
2024-10-27 01:19:39 -0600

    Refactor username input label and validation in UsernameStep component

--- src/components/register/UsernameStep.tsx
+++ src/components/register/UsernameStep.tsx
@@ -8,19 +8,19 @@ const UsernameStep = () => {
   return (
     <div>
       <label htmlFor="username" className="block mb-2 text-sm font-medium text-gray-900">
-        Nombre
+        Nombre de Usuario
       </label>
       <input
         type="text"
         id="username"
-        {...register('firstName', newUserValidationRules.username)}
+        {...register('username', newUserValidationRules.username)}
         className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-        placeholder="John"
+        placeholder="John123"
         required
         autoFocus
         autoComplete="username"
       />
-      {errors.firstName && <span className="text-red-500">{String(errors.firstName.message)}</span>}
+      {errors.username && <span className="text-red-500">{String(errors.username.message)}</span>}
     </div>
   );
 }
