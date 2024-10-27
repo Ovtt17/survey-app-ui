66643ddcd769d34823e950b0ed9d7fffe72f02c4
Ovett Mora <ovettmora17@gmail.com>
2024-10-27 02:09:34 -0600

    refactor: use UserInputField for email input

--- src/components/register/EmailStep.tsx
+++ src/components/register/EmailStep.tsx
@@ -1,25 +1,17 @@
-import { useFormContext } from "react-hook-form";
 import { newUserValidationRules } from "../../data/newUserValidationRules";
+import UserInputField from "./UserInputField";
 
 const EmailStep = () => {
-  const { register, formState: { errors } } = useFormContext();
-
   return (
-    <div>
-      <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900">
-        Correo Electrónico
-      </label>
-      <input
-        type="email"
-        id="email"
-        {...register('email', newUserValidationRules.email)}
-        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-        placeholder="john@email.com"
-        required
-        autoComplete="email"
-      />
-      {errors.email && <span className="text-red-500">{String(errors.email.message)}</span>}
-    </div>
+    <UserInputField
+      id="email"
+      type="email"
+      label="Correo Electrónico"
+      placeholder="john@mail.com"
+      validationRules={newUserValidationRules.email}
+      autoComplete="email"
+      autoFocus
+    />
   );
 }
 
