a2683cd8c1f7d0ea7a0a54b037b98f2fbc7df0c7
Ovett Mora <ovettmora17@gmail.com>
2024-10-27 02:09:48 -0600

    Refactor: Use UserInputField component for username input in registration form

--- src/components/register/UsernameStep.tsx
+++ src/components/register/UsernameStep.tsx
@@ -1,27 +1,17 @@
-import { useFormContext } from "react-hook-form";
 import { newUserValidationRules } from "../../data/newUserValidationRules";
+import UserInputField from './UserInputField';
 
 const UsernameStep = () => {
-
-  const { register, formState: { errors } } = useFormContext();
-
   return (
-    <div>
-      <label htmlFor="username" className="block mb-2 text-sm font-medium text-gray-900">
-        Nombre de Usuario
-      </label>
-      <input
-        type="text"
-        id="username"
-        {...register('username', newUserValidationRules.username)}
-        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-        placeholder="John123"
-        required
-        autoFocus
-        autoComplete="username"
-      />
-      {errors.username && <span className="text-red-500">{String(errors.username.message)}</span>}
-    </div>
+    <UserInputField
+      id="username"
+      type="text"
+      label="Nombre de Usuario"
+      placeholder="John123"
+      validationRules={newUserValidationRules.username}
+      autoComplete="username"
+      autoFocus
+    />
   );
 }
 
